6fbe3b6b0259c2110a1a54513e4ce35a85fa4471
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2023-02-17 13:21:16 -0600

    created navbar

--- src/main/java/com/codeup/codeupspringblog/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/HomeController.java
@@ -11,4 +11,6 @@ public class HomeController {
     public String welcome() {
         return "home";
     }
+
+
 }
---
+++ src/main/java/com/codeup/codeupspringblog/RollDice.java
@@ -0,0 +1,28 @@
+package com.codeup.codeupspringblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class RollDice {
+    @GetMapping("/roll-dice")
+    public String rollDice(){
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{num}")
+    public String userInput(@PathVariable int num, Model model) {
+        int randomNum = (int) Math.floor(Math.random() * 7);
+
+        boolean result = num == randomNum;
+
+        model.addAttribute("result" , result);
+        model.addAttribute("randomNum" , randomNum);
+        model.addAttribute("userGuess", num);
+
+        return "roll-dice";
+    }
+
+}
